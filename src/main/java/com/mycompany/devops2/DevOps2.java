d388768470df45fe3c741c4ccd8d0e15fdb6d2c2
Negrie17 <220757534@ump.ac.za>
2024-05-08 14:21:35 +0200

    My first commit

---
+++ src/main/java/com/mycompany/devops2/DevOps2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devops2;
+
+/**
+ *
+ * @author Student
+ */
+public class DevOps2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World uhiuhuiyiu!");
+        
+    }
+}
